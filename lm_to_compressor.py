34250562f0f42c9d403dcaab035d811f1d42c077
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2025-02-18 14:00:25 -0800

    Take some suggestions on the compressor
    Added a more reasonable scaling factor; tqdm; a few TODOs

--- lm_to_compressor.py
+++ lm_to_compressor.py
@@ -1,4 +1,5 @@
-import transformers, torch, contextlib, importlib
+import transformers, torch, contextlib, importlib, math
+from tqdm import tqdm
 
 arithmeticcoding = importlib.import_module("Reference-arithmetic-coding.python.arithmeticcoding")
 
@@ -6,6 +7,7 @@ gpt2_model = transformers.GPT2LMHeadModel.from_pretrained("gpt2")
 gpt2_tokenizer = transformers.GPT2Tokenizer.from_pretrained("gpt2")
 ALPHABET_SIZE = 50257
 CONTEXT_LENGTH = 1024
+SCALING_FACTOR = 10000
 
 def p_given(condition, model):
 	input_ids = torch.tensor(condition).unsqueeze(0)
@@ -13,22 +15,23 @@ def p_given(condition, model):
 		outputs = model(input_ids, labels=input_ids)
 	token_log_prob = torch.log_softmax(outputs.logits[0, -1], dim=-1)
 	prob = torch.exp(token_log_prob)
-	scaling_factor = 1/torch.min(prob)
-	freqs = prob * scaling_factor
-	return [round(float(i)) for i in freqs] + [1]
+	#scaling_factor = 1/torch.min(prob)
+	freqs = prob * SCALING_FACTOR
+	return [math.ceil(float(i)) for i in freqs] + [1]
 
 
 def compress(inp, bitout):
-	inp = gpt2_tokenizer.encode(inp.decode(), return_tensors="pt")[0]
-	
+	inp = gpt2_tokenizer.encode(gpt2_tokenizer.bos_token+inp.decode(), return_tensors="pt")[0]
 	initfreqs = arithmeticcoding.FlatFrequencyTable(ALPHABET_SIZE+1)
 	freqs = arithmeticcoding.SimpleFrequencyTable(initfreqs)
 	enc = arithmeticcoding.ArithmeticEncoder(32, bitout)
-	for i in range(len(inp)):
+	# TODO: Use BOS token instead of initial uniform distr. and EOS token instead of ALPHABET_SIZE
+	# TODO: Infer from entire sequence for batch processing? 
+	for i in tqdm(range(len(inp))):
 		enc.write(freqs, inp[i].item())
 		context = inp[:i+1]
 		if len(context) > CONTEXT_LENGTH: 
-			context = context[len(context)-CONTEXT_LENGTH]
+			context = context[len(context)-CONTEXT_LENGTH:]
 		new_freqs = p_given(inp[:i+1], gpt2_model)
 		for i in range(ALPHABET_SIZE+1): 
 			freqs.set(i, new_freqs[i])
@@ -74,5 +77,5 @@ def decompress_file(input_path, output_path):
 		contextlib.closing(arithmeticcoding.BitOutputStream(open(output_path, "wb"))) as bitout:
 		decompress(inp, bitout)
 
-compress_file("texts/sentence.txt", "output.bin")
-decompress_file("output.bin", "restore.txt")
+compress_file("texts/paper.txt", "tests/lm_output.bin")
+# decompress_file("output.bin", "restore.txt")
