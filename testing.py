f5d28374820457b8602f2a53e887544bd78bb748
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2025-03-09 22:09:11 -0700

    Write wandb compression rate testing

---
+++ testing.py
@@ -0,0 +1,29 @@
+import wandb, subprocess, os
+
+TEXT = "paper"
+DO_WANDB = True
+
+def compress_rate(inp: str): 
+    out_path = "tests/lm_output.bins"
+    subprocess.run(["python3", "lm_to_compressor.py", "-c", inp, "tests/lm_output.bins"])
+    in_size = os.path.getsize(inp)
+    out_size = sum([os.path.getsize(os.path.join(out_path, f)) for f in os.listdir(out_path) if os.path.isfile(os.path.join(out_path, f))])
+    print(in_size, out_size)
+    if DO_WANDB: wandb.log({"input_size": in_size, "output_size": out_size, "compression_rate": out_size/in_size})
+    return out_size / in_size
+
+
+
+if DO_WANDB: 
+    run = wandb.init(
+        entity="andy-and-only-andy",
+        project="lm-data-compressor",
+        config={
+            "notes": f"Decompress {TEXT}"
+            
+        },
+    )
+    
+
+rate = compress_rate(f"texts/{TEXT}.txt")
+print(rate)
