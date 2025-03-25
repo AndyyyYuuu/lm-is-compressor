d2ffa7ff3b0dfd60001bcc16857a91510317673e
Andy <77585612+AndyyyYuuu@users.noreply.github.com>
2025-03-24 21:00:47 -0700

    Integrate model wrapper into `lm_to_compressor.py`

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class LMWrapper(ABC):
 
     @property
     @abstractmethod
-    def VOCAB_SIZE() -> int:
+    def ALPHABET_SIZE() -> int:
         pass
 
     @property
@@ -14,7 +14,11 @@ class LMWrapper(ABC):
         pass
 
     @abstractmethod
-    def tokenize(condition: torch.Tensor, kv_cache=None: torch.Tensor): 
+    def tokenize(condition: torch.Tensor): 
+        pass
+
+    @abstractmethod
+    def detokenize(condition: torch.Tensor): 
         pass
     
     @abstractmethod
@@ -22,26 +26,32 @@ class LMWrapper(ABC):
         pass
 
     @abstractmethod
-    def p_given(condition: torch.Tensor, kv_cache=None: torch.Tensor) -> torch.Tensor, torch.Tensor: 
+    def p_given(condition: torch.Tensor, kv_cache: torch.Tensor = None) -> torch.Tensor: 
         pass
 
 
 class GPT2(LMWrapper): 
     
-    def __init__():
+    def __init__(self):
         self.model = transformers.GPT2LMHeadModel.from_pretrained("gpt2")
         self.tokenizer = transformers.GPT2Tokenizer.from_pretrained("gpt2")
         self.model.eval()
     
     @property
-    def BOS_TOKEN(): 
+    def BOS_TOKEN(self): 
         return self.tokenizer.bos_token_id
     
     @property
-    def VOCAB_SIZE():
+    def ALPHABET_SIZE(self):
         return 50257
     
-    def batch_p_given(condition: torch.Tensor) -> torch.Tensor:
+    def tokenize(self, inp: str) -> torch.Tensor:
+        return self.tokenizer.encode(inp.read().decode("utf-8"), return_tensors="pt")[0]
+    
+    def detokenize(self, inp: int) -> str:
+        return self.tokenizer.decode(inp)
+    
+    def batch_p_given(self, condition: torch.Tensor) -> torch.Tensor:
         # Condition is a tensor of shape (sequence length, )
         input_ids = condition.unsqueeze(0)
 
@@ -51,13 +61,12 @@ class GPT2(LMWrapper):
         prob = torch.softmax(outputs.logits.squeeze(0), dim=-1)
         return prob
 
-    def p_given(input_id, kv_cache=None) -> torch.Tensor, torch.Tensor:
+    def p_given(self, input_id, kv_cache=None) -> torch.Tensor:
         input_id = input_id.unsqueeze(0)
         with torch.no_grad():
             outputs = self.model(input_id, past_key_values=kv_cache, use_cache=True)
             kv_cache = outputs.past_key_values
 
-        token_log_prob = torch.softmax(outputs.logits[0, -1], dim=-1)
+        prob = torch.softmax(outputs.logits[0, -1], dim=-1)
 
         return prob, kv_cache
-
